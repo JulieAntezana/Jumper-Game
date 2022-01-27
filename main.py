3bd501281931fd1905add956309084afb6ad16ad
marcusBYUI <ojo18002@byui.edu>
2022-01-27 22:23:44 +0100

    jumper game design

---
+++ main.py
@@ -0,0 +1,31 @@
+from words import word_list
+
+"""
+parachutes(5 wrong gueses)
+    returns an image either wrong or right
+    
+word(
+    func: new_word  
+    func: check_guess(
+        if right: parachute
+                    display.update()
+                    
+    )
+)
+display(
+    func: word_tiles(get lenght of new word to create the tiles)
+    func: word_display(
+        display parachute and the current_word tiles
+    )
+    func: update
+)
+jumper(
+    score = 5
+    while loop
+    func: display
+    input
+    func: check_guess
+    
+)
+
+"""
