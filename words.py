d997d75b1488376b1cede096fd20b254290e47a2
marcusBYUI <ojo18002@byui.edu>
2022-01-30 17:33:31 +0100

    generated the basic code for the word class

--- words.py
+++ words.py
@@ -1,4 +1,5 @@
 from word_list import list
+import random
 
 """
     word( **marcus
@@ -10,3 +11,41 @@ from word_list import list
     )
 )
 """
+
+class word:
+    """
+    
+        init method:
+            This method initializes the class with an empty list to store the users guesses to prevent repeatition
+            It also initializes with a new word to be guessed for that particular round of game
+    
+    
+        check_guess method:
+            This funtion checks the user guess, it receives the letter the user guessed as an argument
+                First If&else checks if the user has guessed the word before,
+                    if so returns False
+                
+                
+                Second If&else checks if the letters are present in the current word to be guessed
+                    if present: it calls the display funtion with the letters that would be updates plus setting update to false
+    """
+    
+    
+    def __init__(self):
+        
+        self.users_guess=[]
+        self.new_word = random.choice(list)
+        
+    def check_guess(self, letter):       
+
+        if letter in self.users_guess:
+            return False
+        else:
+            self.users_guess.append(letter)
+            
+            
+        if letter in self.new_word:
+            pass #call the display funtion with the letters that would be updated
+        
+        else:
+            pass #call the display funtion setting update to false
